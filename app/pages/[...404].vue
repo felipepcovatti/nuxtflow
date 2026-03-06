ce712f0ae1d1d807244c71a407e2e5d1f75b3d3c
Felipe <felipepcovatti@gmail.com>
2026-03-06 17:37:30 -0300

    stash-mar-6

--- app/pages/[...404].vue
+++ app/pages/[...404].vue
@@ -1,8 +1,8 @@
 <template>
   <UiCard :title="$route.path" subtitle="Not implemented" class="h-full">
-    <p class="mt-8 text-center">
+    <p class="text-center">
       Only the
-      <NuxtLink to="/" class="font-semibold text-white underline"
+      <NuxtLink to="/" class="font-semibold text-green-400 underline"
         >Overview page</NuxtLink
       >
       content is currently implemented.
