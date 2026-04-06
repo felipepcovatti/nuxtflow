fe62737a3d4fe88f5af6e433e49ee8aae9a522ce
Felipe <felipepcovatti@gmail.com>
2026-04-06 15:35:16 -0300

    refactor: remove empty pages and rely on catch-all 404 route

---
+++ app/pages/[...404].vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const router = useRouter();
+
+function goBack() {
+  if (window.history.state?.back) {
+    router.back();
+  } else {
+    navigateTo("/");
+  }
+}
+</script>
+
+<template>
+  <UiCard :title="$route.path" :subtitle="$t('notImplemented')" class="h-full">
+    <div class="flex flex-col justify-center gap-6 p-4">
+      <p class="text-center">
+        {{ $t("onlyOverviewAvailable") }}
+      </p>
+      <button class="button self-center" @click="goBack">
+        <Icon name="mdi:arrow-left" size="1.25rem" />
+        {{ $t("goBack") }}
+      </button>
+    </div>
+  </UiCard>
+</template>
--- app/pages/pages/chat-room.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <h1 class="text-lg">Chat Room</h1>
-  [not implemented]
-</template>
