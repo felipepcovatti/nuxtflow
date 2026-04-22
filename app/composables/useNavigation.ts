a0a87631a91e04a471bbeac42c5ca94441121c8f
Felipe <felipepcovatti@gmail.com>
2026-04-22 10:36:44 -0300

    refactor(useNavitgation): prevent reactivity problem on active item detection

--- app/composables/useNavigation.ts
+++ app/composables/useNavigation.ts
@@ -3,10 +3,6 @@ import { NAVIGATION_ITEMS, type NavigationItem } from "~/constants/navigation";
 export const useNavigation = () => {
   const route = useRoute();
 
-  const activeItem = computed(() =>
-    NAVIGATION_ITEMS.find((item) => getItemState(item) === "active"),
-  );
-
   const expandedItems = ref<string[]>(
     NAVIGATION_ITEMS.flatMap((item) => (item.subItems ? [item.path] : [])),
   );
@@ -21,9 +17,12 @@ export const useNavigation = () => {
   }
 
   function expandActiveItem() {
-    if (activeItem.value?.subItems) {
+    const activeItem = NAVIGATION_ITEMS.find(
+      (item) => getItemState(item) === "active",
+    );
+    if (activeItem?.subItems) {
       expandedItems.value = Array.from(
-        new Set(expandedItems.value).add(activeItem.value.path),
+        new Set(expandedItems.value).add(activeItem.path),
       );
     }
   }
