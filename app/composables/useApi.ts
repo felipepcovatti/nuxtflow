b313070cef2e32f7edeff1de65342784b3c8f713
Felipe <felipepcovatti@gmail.com>
2026-05-05 16:15:39 -0300

    refactor(useApi): avoid headers reassignment in request interception

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -4,9 +4,7 @@ export const useApi = createUseFetch(() => {
   const referenceTime = useState("referenceTime", () => formatISO(new Date()));
   return {
     onRequest({ options }) {
-      const headers = new Headers(options.headers || {});
-      headers.set("x-reference-time", referenceTime.value);
-      options.headers = headers;
+      options.headers.set("x-reference-time", referenceTime.value);
     },
   };
 });
