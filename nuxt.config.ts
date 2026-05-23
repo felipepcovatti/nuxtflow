59800de87b8eb675def15255a3efacf275d66034
Felipe <felipepcovatti@gmail.com>
2026-05-23 16:34:25 -0300

    chore: remove unneeded 'any' casting from nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [tailwindcss() as any],
+    plugins: [tailwindcss()],
   },
   modules: [
     "reka-ui/nuxt",
